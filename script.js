34544b68ef1167cef38a6efa70216cf780eb560f
JuanFelipe <juanfelipeospinaluna@gmail.com>
2023-04-17 13:49:58 -0500

    encriptador de texto

---
+++ script.js
@@ -0,0 +1,44 @@
+function encriptar() {
+    var texto = document.getElementById("inputtexto").value.toLowerCase();
+    //i es para que afecte a tanto mayus como minus
+    //g afecta a toda la linea
+    //m afecta a multiples lineas
+    var txtcifrado = texto.replace(/e/igm,"enter");
+    var txtcifrado = txtcifrado.replace(/o/igm,"ober");
+    var txtcifrado = txtcifrado.replace(/i/igm,"imes");
+    var txtcifrado = txtcifrado.replace(/a/igm,"ai");
+    var txtcifrado = txtcifrado.replace(/u/igm,"ufat");
+    document.getElementById("img-muñeco").style.display = "none";
+    document.getElementById("mensaje").style.display = "none";
+    document.getElementById("texto-2").style.display = "none";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("output-texto").innerHTML = txtcifrado;
+    document.getElementById("inputtexto").innerHTML = " ";
+    document.getElementById("copiar").style.display = "inherit";
+  }
+  
+  function desencriptar() {
+    var texto = document.getElementById("inputtexto").value.toLowerCase();
+    var txtcifrado = texto.replace(/enter/igm,"e");
+    var txtcifrado = txtcifrado.replace(/ober/igm,"o");
+    var txtcifrado = txtcifrado.replace(/imes/igm,"i");
+    var txtcifrado = txtcifrado.replace(/ai/igm,"a");
+    var txtcifrado = txtcifrado.replace(/ufat/igm,"u");
+    document.getElementById("img-muñeco").style.display = "none";
+    document.getElementById("mensaje").style.display = "none";
+    document.getElementById("texto-2").style.display = "none";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("output-texto").innerHTML = txtcifrado;
+    document.getElementById("inputtexto").innerHTML = " ";
+    document.getElementById("copiar").style.display = "inherit";
+  }
+  
+  function copiar() {
+    var contenido = document.querySelector("#output-texto");
+    contenido.select();
+    document.execCommand('copy');
+    alert("Copiado!");
+  }
+  
+
+  //Realizado por Juan Felipe Ospina Luna / Grupo 5
